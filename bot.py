6d64ba08a0738ebfb3e1d3da57c43cf947cd22e4
kemisola1 <76532563+kemisola1@users.noreply.github.com>
2022-08-26 15:55:19 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+class TableScraper:
+    results = []
+
+    def fetch(self, url):
+        return requests.get(url)
+
+    def parse(self, html):
+        content = BeautifulSoup(html, 'lxml')
+        table = content.find('table')
+        rows = table.findAll('tr')
+        self.results.append([header.text for header in rows[0].findAll('tr')])
+
+        for row in rows:
+            if len(row.findAll('td')):
+               self.results.append([data.text for data in row.findAll('td')])
+
+
+    def to_csv(self):
+        with open('table.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerows(self.results)
+
+    def run(self):
+        response =self.fetch('URL')
+        self.parse(response.text)
+        self.to_csv()
+
+if __name__== '__main__':
+    scraper = TableScraper()
+    scraper.run()
+
